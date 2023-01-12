a222dfecfcdbef56aab33e87cafd990c5068e6d6
deepak7691 <119676343+deepak7691@users.noreply.github.com>
2023-01-12 13:32:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h1 data-ns-test="project-name">musicApp</h1>
+     <p data-ns-test="project-description">My first Project</p>
+    </div>
   )
 }
 
